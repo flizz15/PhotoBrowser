697e42313826cd507b1df7aa4eca7dd9043ce28d
Jan Sadownik <30599336+flizz15@users.noreply.github.com>
2020-04-01 14:29:11 +0200

    added css and logic

--- PhotoBrowser.py
+++ PhotoBrowser.py
@@ -4,20 +4,40 @@ from glob import glob
 
 def photo_browser():
     all_container_classes = 'm-2 p-2 bg-gray-500'
-    small_img_container_classes = all_container_classes + ' inline-flex'
+    images_container_style = 'display:flex; justify-content:space-evenly; flex-wrap: wrap;'
+    small_img_container_style = 'background-color: gray;'
     main_img_style = f'width: {1920 / 2.4}px; display: block; margin-left: auto; margin-right: auto;'
     small_img_style = f'width: {1920 / 2.4 / 4}px;'
 
+    def small_container_mouseenter(self, msg):
+        msg.page.main_img.src = self.child.src
+        self.child.style += ' transform: scale(.85);'
+
+    def small_container_mouseleave(self, msg):
+        self.child.style = small_img_style
+
+    def show_main_container_mouseleave(self, msg):
+        msg.page.main_img.src = msg.page.default_img
+
     wp = jp.WebPage()
 
+    default_img = '/static/photos/B9ARSkuWrMA.jpg'
     main_img_container = jp.Div(a=wp, classes=all_container_classes)
-    main_img = jp.Img(src='/static/photos/B9ARSkuWrMA.jpg', a=main_img_container, style=main_img_style)
+    main_img = jp.Img(src=default_img, a=main_img_container, style=main_img_style)
 
-    images_container = jp.Div(a=wp, classes=all_container_classes)
+    images_container = jp.Div(a=wp, classes=all_container_classes, mouseleave=show_main_container_mouseleave,
+                              style=images_container_style)
     small_images = glob('photos/*')
+
+    wp.main_img = main_img
+    wp.default_img = default_img
+
     for i in range(len(small_images)):
-        small_img_container = jp.Div(a=images_container, classes=small_img_container_classes, style='background-color: gray;')
-        jp.Img(src=f'/static/{small_images[i]}', a=small_img_container, style=small_img_style)
+        small_img_container = jp.Div(a=images_container, classes=all_container_classes,
+                                     mouseenter=small_container_mouseenter, mouseleave=small_container_mouseleave,
+                                     style=small_img_container_style)
+        small_img = jp.Img(src=f'/static/{small_images[i]}', a=small_img_container, style=small_img_style, )
+        small_img_container.child = small_img
 
     return wp
 
